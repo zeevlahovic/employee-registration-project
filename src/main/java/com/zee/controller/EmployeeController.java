a78b7f618cc5571cce7df2b8b216ed1928a43a63
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-02 11:21:03 -0400

    Employee Registration Page

--- src/main/java/com/zee/controller/EmployeeController.java
+++ src/main/java/com/zee/controller/EmployeeController.java
@@ -5,10 +5,13 @@ import com.zee.model.Employee;
 import com.zee.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -22,7 +25,7 @@ public class EmployeeController {
 
 
     @GetMapping("/register")
-    public String createEmployee(Model model) {
+    public String createEmployee( Model model) {
 
         model.addAttribute("employee", new Employee());
         model.addAttribute("stateList", DataGenerator.getAllStates());
@@ -31,7 +34,11 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()){
+            return "employee/employee-create";
+        }
 
         employeeService.saveEmployee(employee);
 
--- src/main/java/com/zee/model/Employee.java
+++ src/main/java/com/zee/model/Employee.java
@@ -3,6 +3,9 @@ package com.zee.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,7 +15,13 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//@NonNull --->    Field shouldn't be null
+//@NotEmpty --->   Field shouldn't be ""
+//@NotBlank --->    Field shouldn't be "       "
 
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
 
