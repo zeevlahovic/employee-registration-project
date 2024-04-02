4977ae918ee7c7b98e9dc1900e4bfa25cc853afd
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-01 23:26:18 -0400

    Employee Registration Page

--- src/main/java/com/zee/controller/EmployeeController.java
+++ src/main/java/com/zee/controller/EmployeeController.java
@@ -1,2 +1,17 @@
-package com.zee.controller;public class EmployeeController {
+package com.zee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String createEmployee(){
+        return "employee/employee-create";
+    }
+
+
 }
--- src/main/java/com/zee/model/Employee.java
+++ src/main/java/com/zee/model/Employee.java
@@ -1,2 +1,27 @@
-package com.zee.model;public class Employee {
+package com.zee.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
 }
